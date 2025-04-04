44d4028cf4760fbaa19e5a8a0f5c96319c92afa7
Drizzle210 <anhduc5a1tp@gmail.com>
2025-04-04 10:56:24 +0700

    fix: proto account bug & simplify PubkeyToCosmosAddress

--- client/debug/debug.go
+++ client/debug/debug.go
@@ -80,7 +80,7 @@ func PubkeyCmd() *cobra.Command {
 				return err
 			}
 
-			cosmosAddress, err := types.PubkeyToAddress(pubkeyString)
+			cosmosAddress, err := types.PubkeyToCosmosAddress(pubkeyString)
 			if err != nil {
 				return err
 			}
