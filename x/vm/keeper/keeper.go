151b9d5ba3af981df1f5e26381eac3d1517745bc
dang.vh <dang.vh2@orai.io>
2025-04-09 18:17:23 +0700

    test ante

--- x/vm/keeper/keeper.go
+++ x/vm/keeper/keeper.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"bytes"
 	"fmt"
 	"math/big"
 
@@ -11,8 +12,11 @@ import (
 	"cosmossdk.io/store/prefix"
 	storetypes "cosmossdk.io/store/types"
 	"github.com/cosmos/cosmos-sdk/codec"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/cosmos/evm/crypto/ethsecp256k1"
 	"github.com/cosmos/evm/x/vm/core/vm"
 	"github.com/cosmos/evm/x/vm/statedb"
 	"github.com/cosmos/evm/x/vm/types"
@@ -394,4 +398,37 @@ func (k Keeper) MigrateBalance(ctx sdk.Context, evmAddress common.Address, mappe
 		}
 	}
 	return nil
+}
+
+func (k Keeper) ValidateSignerAnte(ctx sdk.Context, pk cryptotypes.PubKey, signer sdk.AccAddress) error {
+	accAddressFromPubkey, err := k.GetAccAddressBytesFromPubkey(ctx, pk)
+	if err != nil {
+		return err
+	}
+	// we convert signer AccAddress to evm address because in eip712, the signer is bytes() of evm address
+	evmAddressFromSigner := common.BytesToAddress(signer)
+	signerFromEvmAddressSigner := k.GetCosmosAddressMapping(ctx, evmAddressFromSigner)
+
+	if !bytes.Equal(accAddressFromPubkey, signerFromEvmAddressSigner.Bytes()) {
+		return errorsmod.Wrapf(sdkerrors.ErrorInvalidSigner,
+			"Signer from pubkey %s does not match signer from GetSigners %s", sdk.AccAddress(accAddressFromPubkey).String(), signerFromEvmAddressSigner.String())
+	}
+	return nil
+}
+
+func (k Keeper) GetAccAddressBytesFromPubkey(ctx sdk.Context, pk cryptotypes.PubKey) ([]byte, error) {
+	var addressFromPubkey []byte
+	if pk.Type() == ethsecp256k1.KeyType {
+		evmAddressFromPubkey, err := types.PubkeyBytesToEVMAddress(pk.Bytes())
+		if err != nil {
+			return nil, errorsmod.Wrapf(sdkerrors.ErrInvalidPubKey,
+				"Pubkey is invalid to convert to evm address: %s", pk.String())
+		}
+		signerFromPubkey := k.GetCosmosAddressMapping(ctx, *evmAddressFromPubkey)
+		addressFromPubkey = signerFromPubkey.Bytes()
+		return addressFromPubkey, nil
+	}
+
+	addressFromPubkey = pk.Address().Bytes()
+	return addressFromPubkey, nil
 }
