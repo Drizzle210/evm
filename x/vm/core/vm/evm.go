e2c9be150c2c748057863ad9b451034cc7efd8b6
ledanghuy1811 <huygamo567@gmail.com>
2025-04-23 13:57:01 +0700

    fix: pass readOnly to evm call

--- x/vm/core/vm/evm.go
+++ x/vm/core/vm/evm.go
@@ -225,7 +225,7 @@ func (evm *EVM) Call(caller ContractRef, addr common.Address, input []byte, gas 
 
 	// It is allowed to call precompiles, even via call -- as opposed to callcode, staticcall and delegatecall it can also modify state
 	if isPrecompile {
-		ret, gas, err = evm.RunPrecompiledContract(p, caller, input, gas, value, false)
+		ret, gas, err = evm.RunPrecompiledContract(p, caller, input, gas, value, evm.interpreter.ReadOnly())
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
 		// The contract is a scoped environment for this execution context only.
@@ -293,7 +293,7 @@ func (evm *EVM) CallCode(caller ContractRef, addr common.Address, input []byte, 
 	// It is allowed to call precompiles, even via callcode, but only for reading
 	contract := NewContract(caller, AccountRef(caller.Address()), value, gas)
 	if p, isPrecompile := evm.Precompile(addr); isPrecompile {
-		ret, gas, err = evm.RunPrecompiledContract(p, AccountRef(contract.CallerAddress), input, gas, value, true)
+		ret, gas, err = evm.RunPrecompiledContract(p, AccountRef(contract.CallerAddress), input, gas, value, evm.interpreter.ReadOnly())
 	} else {
 		addrCopy := addr
 		// Initialise a new contract and set the code that is to be used by the EVM.
@@ -338,7 +338,7 @@ func (evm *EVM) DelegateCall(caller ContractRef, addr common.Address, input []by
 	// It is allowed to call precompiles, even via delegatecall
 	contract := NewContract(caller, AccountRef(caller.Address()), nil, gas).AsDelegate()
 	if p, isPrecompile := evm.Precompile(addr); isPrecompile {
-		ret, gas, err = evm.RunPrecompiledContract(p, AccountRef(contract.CallerAddress), input, gas, nil, true)
+		ret, gas, err = evm.RunPrecompiledContract(p, AccountRef(contract.CallerAddress), input, gas, nil, evm.interpreter.ReadOnly())
 	} else {
 		addrCopy := addr
 		// Initialise a new contract and make initialise the delegate values
